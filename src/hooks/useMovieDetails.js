4f07b6685784b17c0ca342c44d4c65488629fc2e
himansusahu11 <himansu.sahu11@gmail.com>
2024-07-20 12:06:07 +0530

    fixing a bug called 'destory is not a function' in useMovieList.js hook

--- src/hooks/useMovieDetails.js
+++ src/hooks/useMovieDetails.js
@@ -1,24 +1,56 @@
+// import { useContext, useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+
+// import { LangContext, ThemeContext } from "../App";
+// import { getMovieDetails } from "../api/Movie";
+// export default function useMovieDetails() {
+//   const params = useParams();
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [movieData, setMovieData] = useState(null);
+//   const movieId = params.movieId;
+
+//   const { theme } = useContext(ThemeContext);
+//   const value = useContext(LangContext);
+
+//   const fetchMoviDetails = async () => {
+//     const movieDetails = await getMovieDetails(movieId);
+//     setIsLoading(false);
+//     setMovieData(movieDetails);
+//   };
+//   useEffect(() => {
+//     fetchMoviDetails();
+//   }, []);
+//   return { isLoading, theme, movieData, movieId };
+// }
+
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { LangContext, ThemeContext } from "../App";
 import { getMovieDetails } from "../api/Movie";
+
 export default function useMovieDetails() {
-  const params = useParams();
+  const { movieId } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [movieData, setMovieData] = useState(null);
-  const movieId = params.movieId;
 
   const { theme } = useContext(ThemeContext);
   const value = useContext(LangContext);
 
-  const fetchMoviDetails = async () => {
-    const movieDetails = await getMovieDetails(movieId);
-    setIsLoading(false);
-    setMovieData(movieDetails);
-  };
   useEffect(() => {
-    fetchMoviDetails();
-  }, []);
+    const fetchMovieDetails = async () => {
+      try {
+        const movieDetails = await getMovieDetails(movieId);
+        setMovieData(movieDetails);
+      } catch (error) {
+        console.error("Failed to fetch movie details:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [movieId]);
+
   return { isLoading, theme, movieData, movieId };
 }
--- src/hooks/useMovieList.js
+++ src/hooks/useMovieList.js
@@ -1,16 +1,92 @@
+// import { useContext, useEffect, useState } from "react";
+// import { getAllMovies } from "../api/Movie";
+// import { ThemeContext } from "../App";
+// import useToggle from "./useToggle";
+
+// // import useToggle from "../../hooks/useToggle";
+// var allMovieData = [];
+// export default function useMovieLlist() {
+//   console.log("component rendred again");
+//   const [movieDetails, setMovieDetails] = useState([]);
+//   //controlled form
+
+//   const [isLoading, setIsLoading] = useToggle(true);
+//   const [isLoggedIn, setIsLoggedIn] = useState(true);
+//   const { theme } = useContext(ThemeContext);
+
+//   /**
+//    * useEffectIsTriggered: ComponentDidMount + ComponentDidUpdate
+//    * if we pass an empty array:-= No dependency :-=- it will work on ComponentDidMount
+//    */
+//   const fetchMoviesData = async () => {
+//     const movieData = await getAllMovies();
+//     allMovieData = movieData;
+//     setIsLoading(false);
+//     setMovieDetails(movieData);
+//   };
+//   useEffect(async () => {
+//     fetchMoviesData();
+//   }, []);
+
+//   /*
+//        ComponentDidMount + Every time searchValue is updated
+//     */
+
+//   const onLanguageChange = (languageSelected) => {
+//     if (languageSelected === "all") {
+//       setMovieDetails(allMovieData);
+//       return;
+//     }
+//     const filteredMovies = allMovieData.filter((movie) => {
+//       return movie.language.toLowerCase() === languageSelected;
+//     });
+//     setMovieDetails(filteredMovies);
+//   };
+
+//   function onMovieDelete(id) {
+//     const removeDeletedMovie = movieDetails.filter((movie) => {
+//       return movie._id !== id;
+//     });
+//     setMovieDetails(removeDeletedMovie);
+//   }
+//   // useEffect(() => {
+//   //   console.log("useEffect with dependency on searchValue triggered");
+//   //   //filter movies every time when a searchValue state is changed
+//   //   const filteredMovies = allMovieData.filter((movie) => {
+//   //     return movie.name.toLowerCase().startsWith(searchValue);
+//   //   });
+//   //   setMovieDetails(filteredMovies);
+//   // }, [searchValue]);
+
+//   const filterResult = (searchValue) => {
+//     const filteredMovies = allMovieData.filter((movie) => {
+//       return movie.name.toLowerCase().startsWith(searchValue);
+//     });
+//     setMovieDetails(filteredMovies);
+//   };
+//   return {
+//     theme,
+//     isLoading,
+//     filterResult,
+//     onLanguageChange,
+//     movieDetails,
+//     onMovieDelete,
+//     isLoggedIn,
+//   };
+// }
+
 import { useContext, useEffect, useState } from "react";
 import { getAllMovies } from "../api/Movie";
 import { ThemeContext } from "../App";
 import useToggle from "./useToggle";
 
 // import useToggle from "../../hooks/useToggle";
-var allMovieData = [];
-export default function useMovieLlist() {
-  console.log("component rendred again");
-  const [movieDetails, setMovieDetails] = useState([]);
-  //controlled form
+let allMovieData = [];
 
-  const [isLoading, setIsLoading] = useToggle(true);
+export default function useMovieList() {
+  console.log("component rendered again");
+  const [movieDetails, setMovieDetails] = useState([]);
+  const [isLoading, toggleIsLoading] = useToggle(true);
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const { theme } = useContext(ThemeContext);
 
@@ -18,19 +94,21 @@ export default function useMovieLlist() {
    * useEffectIsTriggered: ComponentDidMount + ComponentDidUpdate
    * if we pass an empty array:-= No dependency :-=- it will work on ComponentDidMount
    */
-  const fetchMoviesData = async () => {
-    const movieData = await getAllMovies();
-    allMovieData = movieData;
-    setIsLoading(false);
-    setMovieDetails(movieData);
-  };
-  useEffect(async () => {
-    fetchMoviesData();
-  }, []);
+  useEffect(() => {
+    const fetchMoviesData = async () => {
+      try {
+        const movieData = await getAllMovies();
+        allMovieData = movieData;
+        setMovieDetails(movieData);
+      } catch (error) {
+        console.error("Failed to fetch movie data:", error);
+      } finally {
+        toggleIsLoading(false);
+      }
+    };
 
-  /*
-       ComponentDidMount + Every time searchValue is updated
-    */
+    fetchMoviesData();
+  }, [toggleIsLoading]);
 
   const onLanguageChange = (languageSelected) => {
     if (languageSelected === "all") {
@@ -43,20 +121,12 @@ export default function useMovieLlist() {
     setMovieDetails(filteredMovies);
   };
 
-  function onMovieDelete(id) {
+  const onMovieDelete = (id) => {
     const removeDeletedMovie = movieDetails.filter((movie) => {
       return movie._id !== id;
     });
     setMovieDetails(removeDeletedMovie);
-  }
-  // useEffect(() => {
-  //   console.log("useEffect with dependency on searchValue triggered");
-  //   //filter movies every time when a searchValue state is changed
-  //   const filteredMovies = allMovieData.filter((movie) => {
-  //     return movie.name.toLowerCase().startsWith(searchValue);
-  //   });
-  //   setMovieDetails(filteredMovies);
-  // }, [searchValue]);
+  };
 
   const filterResult = (searchValue) => {
     const filteredMovies = allMovieData.filter((movie) => {
@@ -64,6 +134,7 @@ export default function useMovieLlist() {
     });
     setMovieDetails(filteredMovies);
   };
+
   return {
     theme,
     isLoading,
